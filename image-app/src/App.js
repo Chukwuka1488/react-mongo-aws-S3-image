e4f19ce8fb15d52f94567aa061c1785d9a883b6e
Chuks Akibot <juniorakibor@yahoo.com>
2021-10-06 18:48:05 +0200

    using useref hooks to trigger popup during image selection

--- image-app/src/App.js
+++ image-app/src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
 
+
 function App() {
+  // to trigger the pop-up when trying to select image: use  useRef react hooks
+  const InputRef = React.useRef();
+
+  const triggerFileSelectPopup = () => InputRef.current.click();
+
   return (
     <div className='container'>
       <div className='container-cropper'>
         <div className='container-buttons'>
-          <input type='file' accept='image/*' />
+          <input
+            type='file'
+            accept='image/*'
+            ref={InputRef}
+            style={{ display: 'none' }}
+          />
           <Button variant='contained' color='primary'>
             Choose
           </Button>
