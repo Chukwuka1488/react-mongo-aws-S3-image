5fa777545852d25d659c832c9c3bfb9c8a6b46bc
Chuks Akibot <juniorakibor@yahoo.com>
2021-10-06 19:00:04 +0200

    adding states and props functionalities

--- image-app/src/App.js
+++ image-app/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
-
+import Cropper from 'react-easy-crop';
+import Slider from '@material-ui/core/Slider';
 
 function App() {
   // to trigger the pop-up when trying to select image: use  useRef react hooks
@@ -10,15 +11,36 @@ function App() {
 
   const triggerFileSelectPopup = () => InputRef.current.click();
 
+  // create state
+  const [image, setImage] = React.useState(null);
+  const [croppedArea, setCroppedArea] = React.useState(null);
+  const [crop, setCrop] = React.useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = React.useState(1);
+
+  const onCropIsComplete = () => {};
+
+  const onSelectFile = () => {};
+
   return (
     <div className='container'>
       <div className='container-cropper'>
+        <Cropper
+          image={image}
+          crop={crop}
+          zoom={zoom}
+          aspect={1}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          onCropComplete={onCropIsComplete}
+        />
+        <Slider />
         <div className='container-buttons'>
           <input
             type='file'
             accept='image/*'
             ref={InputRef}
             style={{ display: 'none' }}
+            onChange={onSelectFile}
           />
           <Button variant='contained' color='primary'>
             Choose
