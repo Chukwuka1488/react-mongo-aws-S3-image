11bc3fc116ed0188f553e7f1b070cb17f020ca7c
Chuks Akibot <juniorakibor@yahoo.com>
2021-10-06 18:37:27 +0200

    material ui button created

--- image-app/src/App.js
+++ image-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='container-cropper'>
+        <div className='container-buttons'>
+          <Button variant='contained' color='primary'>Choose</Button>
+          <Button variant='contained' color='secondary'>Download</Button>
+        </div>
+      </div>
     </div>
   );
 }
